dd468dd41197c94711c9fc5e55b43453c95ce3f7
Ihechineme <ezehanastasia66@gmail.com>
2022-07-04 15:48:37 -0700

    Correction

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point of code
+ *
+ * Return: 0 if code is successful
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return(0)
+}
