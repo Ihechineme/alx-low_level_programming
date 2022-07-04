03249c37860f72636eb9f2787f5ead6793bd4606
Ihechineme <ezehanastasia66@gmail.com>
2022-07-04 15:19:46 -0700

    Correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * The function main indicates the entry point
+ * Return 0 indicates that the function is a success
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
